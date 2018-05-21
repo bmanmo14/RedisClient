f1108a7c3087a6494653e3f189bb050963003c5b
EC2 Default User <ec2-user@ip-172-31-28-84.us-west-2.compute.internal>
2018-05-21 22:44:27 +0000

    Change some ish, got it working on EC2

--- RedisClient/src/RedisClient.java
+++ RedisClient/src/RedisClient.java
@@ -17,9 +17,9 @@ public class RedisClient {
 	public static void main(String[] args) {
 		// Logging info created to log failover response to file
 		FileHandler fh;
-		Logger logger = Logger.getLogger(RedisClient.class.getName());
+		final Logger logger = Logger.getLogger(RedisClient.class.getName());
 		try {
-			fh = new FileHandler("/Users/bmouser/Documents/RedisClient/logger.log");
+			fh = new FileHandler("/home/ec2-user/RedisClient/RedisClient/src/logger.log");
 			SimpleFormatter formatter = new SimpleFormatter();  
 	        fh.setFormatter(formatter);
 	        logger.addHandler(fh);
@@ -30,20 +30,22 @@ public class RedisClient {
 		} 
 		
 		int failed = 0;
-		
+		//System.out.println("Got Here1");
 		// Create a list of sentinels that are currently running on the master at their given ports
 		Set<String> sentinels = new HashSet<String>();
-		master = new Jedis("rediscluster.bocodh.0001.usw2.cache.amazonaws.com", 6379);
-		//sentinels.add("127.0.0.1:5000");
-		//sentinels.add("127.0.0.1:5001");
-		//sentinels.add("127.0.0.1:5002");
+		//master = new Jedis("rediscluster.bocodh.clustercfg.usw2.cache.amazonaws.com", 6379);
+		sentinel = new Jedis("127.0.0.1",5000);
+		//System.out.println(master.info());
+		sentinels.add("127.0.0.1:5000");
+		sentinels.add("127.0.0.1:5001");
+		sentinels.add("127.0.0.1:5002");
 		
 		// Create a sentinel pool and access the master via the sentinel pool
-		//JedisSentinelPool sentinel_pool = new JedisSentinelPool("mymaster",sentinels);
-		//master = sentinel_pool.getResource();
+		JedisSentinelPool sentinel_pool = new JedisSentinelPool("mymaster",sentinels);
+		master = sentinel_pool.getResource();
 		
 		// PubSub to listen for failover and log messages from failover
-		JedisPubSub sub = new JedisPubSub() {
+		final JedisPubSub sub = new JedisPubSub() {
 		@Override
 		public void onPMessage(String pattern, String channel, String message) {
 			logger.log(Level.INFO, channel);
@@ -54,24 +56,25 @@ public class RedisClient {
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				//sentinel.psubscribe(sub, "*");
+				sentinel.psubscribe(sub, "*");
 			}
 			
 		}).start();
 		
+		//System.out.println("Got Here2");
 		// Continually write message to the server, then cause failover from outside of client and watch how client responds.
 		while(true) {
-			try{
+		    try{
 			String response = master.set("key","value");
 			if(response.equals("OK")) {
 				System.out.print("\rSuccessful");
 			}
 			}
-			catch(JedisConnectionException jce) {
+						catch(JedisConnectionException jce) {
 				System.out.print("\rFailover Occuring");
 				//master.close();
 				//master = sentinel_pool.getResource();
-			}
+							}
 		}				
 	}
 
